a4e9cf65103cb39915ff5dd3ed9414bad036d493
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-04 11:53:58 +0000

    Names now added from user input and stored, added while method to check when name is entered and when to finish/print list

--- directory.rb
+++ directory.rb
@@ -1,27 +1,32 @@
+def student_input 
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get first name 
+  name = gets.chomp
+  # while name is empty repeat code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we haveve #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
-# first we print the lsit of students
 
-student_list = [
-{name: "Dr. Hanninbal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name: "Joffery Baratheon", cohort: :november},
-{name: "Norman Bates", cohort: :november}
-]
 
 def print_header
   puts "The students of Villains Achademy"
   puts "-------------"
 end
 
-def print_names(student_list)
-  student_list.each do |student|
+# students value taken from student_input return value 
+def print_names(students)
+  students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
@@ -31,6 +36,24 @@ def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
 
+students = student_input
 print_header
-print_names(student_list)
-print_footer(student_list)
+print_names(students)
+print_footer(students)
+
+
+
+## Old values held in arrays
+# student_list = [
+# {name: "Dr. Hanninbal Lecter", cohort: :november},
+# {name: "Darth Vader", cohort: :november},
+# {name: "Nurse Ratched", cohort: :november},
+# {name: "Michael Corleone", cohort: :november},
+# {name: "Alex DeLarge", cohort: :november},
+# {name: "The Wicked Witch of the West", cohort: :november},
+# {name: "Terminator", cohort: :november},
+# {name: "Freddy Krueger", cohort: :november},
+# {name: "The Joker", cohort: :november},
+# {name: "Joffery Baratheon", cohort: :november},
+# {name: "Norman Bates", cohort: :november}
+# ]
