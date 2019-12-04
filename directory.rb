eeef7669080215b0539f7951f22095054280abc2
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-04 11:04:58 +0000

    student_list now hashes k,v pairs. updated each loop taking in k,v values

--- directory.rb
+++ directory.rb
@@ -2,17 +2,17 @@
 # first we print the lsit of students
 
 student_list = [
-"Dr. Hanninbal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alex DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffery Baratheon",
-"Norman Bates"
+{name: "Dr. Hanninbal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffery Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
 ]
 
 def print_header
@@ -20,9 +20,9 @@ def print_header
   puts "-------------"
 end
 
-def print_names(names)
-  names.each do |student|
-    puts student
+def print_names(student_list)
+  student_list.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
