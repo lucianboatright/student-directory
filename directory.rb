62611258234b079fc8203f79b96744e07910514d
Lucian Boatright <lucian.boatright@gmail.com>
2019-12-06 08:39:39 +0000

    added method Save text to file

--- directory.rb
+++ directory.rb
@@ -89,42 +89,30 @@ def process(selection)
   end
 end
 
+def save_students
+  #open file for writing
+  file = File.open("student.csv","w")
+  #itereate over array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
 def interactive_menu
   students = []
   loop do
-    # # 1. print to menu and ask the user what to do
-    # puts "1. Input the student"
-    # puts "2. show the students"
-    # puts "9. Exit" # beacuse 9 we'll be adding more items
-    # 2. read the input and save it into a veariable
     print_menu
     process(gets.chomp)
-    # 3. do what the user has asked 
-    # case selection
-    #   when "1"
-    #     students = student_input
-    #   when "2"
-    #     # print_header
-    #     # print_names(students)
-    #     # print_footer(students)
-    #     # # input statments
-    #   when "9"
-    #     exit # this will terminate the program
-    #   else 
-    #     puts "I don't know what you meant, try again"
-    #     # 4. repeat from step 1
-    # end
   end
 end
 
 
 interactive_menu
 
-# students = student_input
-# print_header
-# print_names(students)
-# print_cohort(students)
-# print_footer(students)
+
 
 
 
